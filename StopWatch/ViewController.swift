c31d626163d7822b7f426326f1bceb043e20e824
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2018-02-05 17:32:25 +0900

    first commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -4,12 +4,18 @@
 //
 //  Created by 上田　護 on 2017/10/28.
 //  Copyright © 2017年 上田　護. All rights reserved.
-//
+//git
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func up(){
+        //countを0.01足す
+        count = count + 0.01
+        
+        label.text = String(format: "%.2f", count)
+    }
 
+    @IBAction func start() {
+        if !timer.isValid {
+            //タイマーが動作していないとき
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true
+            )
+        }
+        
+    }
 
+    @IBAction func stop() {
+        if timer.isValid {
+            //タイマーが動作しているとき
+            timer.invalidate()
+            
+        }
+    }
 }
 
